364fbb5608cbbc5ec3e17d66a3f42709af9c59d0
madruga665 <madruga665@hotmail.com>
2021-06-20 22:25:03 -0300

    deploy github pages

---
+++ build/precache-manifest.b054751f331368bff8c881976240e73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9749c65b187f1267ccf9962796fcd25",
+    "url": "/sd-010-a-project-trybewallet/index.html"
+  },
+  {
+    "revision": "10cf5a76de92eb7caec5",
+    "url": "/sd-010-a-project-trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b007e43fe8bfc7c6be3",
+    "url": "/sd-010-a-project-trybewallet/static/js/2.6734fe52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sd-010-a-project-trybewallet/static/js/2.6734fe52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cf5a76de92eb7caec5",
+    "url": "/sd-010-a-project-trybewallet/static/js/main.6513bdd9.chunk.js"
+  },
+  {
+    "revision": "ea48161839b5492dce93",
+    "url": "/sd-010-a-project-trybewallet/static/js/runtime-main.584dbda6.js"
+  }
+]);
