21727c92c1d31de5a6e2ea42b2c4219902e57f49
Luciano Amâncio <madruga665@hotmail.com>
2021-10-04 04:29:03 -0300

    novo build

---
+++ build/precache-manifest.5cf7cf46a45bcb72e2727669eeeb435d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bc83cd2b42d2a1ebd54f8e37981b8e",
+    "url": "/trybe-projeto-trybewallet/index.html"
+  },
+  {
+    "revision": "746c14454e834190d170",
+    "url": "/trybe-projeto-trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a1634591b498203d51c",
+    "url": "/trybe-projeto-trybewallet/static/js/2.aad722d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-projeto-trybewallet/static/js/2.aad722d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746c14454e834190d170",
+    "url": "/trybe-projeto-trybewallet/static/js/main.cdfd40b7.chunk.js"
+  },
+  {
+    "revision": "bc4010bfddcaac4bf5c8",
+    "url": "/trybe-projeto-trybewallet/static/js/runtime-main.f9201480.js"
+  }
+]);
